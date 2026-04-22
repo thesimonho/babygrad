eba547df42030bd6bb1bdc23507c9f828cf9c2d0
Simon Ho <simonho.ubc@gmail.com>
2026-04-22 12:09:10 -0700

    chore: stubs

---
+++ babygrad/tensor.py
@@ -0,0 +1,16 @@
+class Tensor:
+    def __init__(self, data: list, shape: tuple) -> None:
+        self.data = data
+        self.shape = shape
+
+    def __add__(self):
+        pass
+
+    def __sub__(self):
+        pass
+
+    def __mul__(self):
+        pass
+
+    def __matmul__(self):
+        pass
